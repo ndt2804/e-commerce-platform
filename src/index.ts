a37f75b31d55459b5838e7e6e62aa9de845ff300
ndt2804 <100899825+ndt2804@users.noreply.github.com>
2023-09-19 18:34:35 +0700

    chore(deps): update package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+
+import { createClient } from '@supabase/supabase-js'
 dotenv.config();
+
+console.log(process.env.supabaseUrl);
+
+const supabase = createClient(`${process.env.supabaseUrl}`, `${process.env.supabaseKey}`)
+console.log(supabase);
 const app: Express = express();
 const port = process.env.PORT;
 app.get('/', (req: Request, res: Response) => {
@@ -9,3 +16,5 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+
